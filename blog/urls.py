7fbf2bfa8f96a94854301b994d03b30cd1a2ee1f
karthik-thunga <kota.karthikthunga803@gmail.com>
2020-05-10 16:02:41 +0530

    Feeds are added

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from . import views
+from .feeds import PostRssFeed
 
 # Contains All urls related to blog app
 
@@ -10,4 +11,5 @@ urlpatterns = [
     path('tags/<slug:tag_slug>/',views.post_list, name='blog_list_by_tags'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
+    path('feed/', PostRssFeed(), name='post_feed'),
 ]
